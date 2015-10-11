a270f7d579de0b4194858ff4d39819471617587b
pooleside <lpoole26@gmail.com>
2015-10-10 18:14:24 -0700

    "add .json file and import initial data into firebase"

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,7 @@
 import Ember from 'ember';
 
-var questions = [{
-  author: "Nevada Nelly",
-  title: "Best places to gamble",
-  body: "My favorite game is roulette.  Where can I find a place to play in Oregon?"
-}, {
-
-  author: "Bruce Williams",
-  title: "Pizza places in Cannon Beach",
-  body: "Where can I find deep-dish pizza on the coast?"
-
-}, {
-
-  author: "Gertie Small",
-  title: "Lawn mower repair",
-  body: "I have an old 1980 Honda mower.  Where can I get it serviced?"
-}];
-
 export default Ember.Route.extend({
   model () {
-    return questions;
-  }
+    return this.store.findAll('question');
+  },
 });
