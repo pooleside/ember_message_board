42574f7fd43a9c62226e17b5755d3d2e9ce0ae1b
pooleside <lpoole26@gmail.com>
2015-10-14 15:48:11 -0700

    "add save function"

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    save() {
+      var params = {
+        title: this.get('title') ? this.get('title') : "",
+        author: this.get('author') ? this.get('author') : "",
+        body: this.get('body') ? this.get('body') : "",
+      };
+      this.set('addNewQuestion', false),
+      this.sendAction('save', params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -7,6 +7,11 @@ export default Ember.Route.extend({
   },
 
   actions: {
+    save(params) {
+      var newQuestion=this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
     delete(question) {
       if(confirm('Are you sure you want to delete this question?')){
       question.destroyRecord();
