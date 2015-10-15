b5a84498d60c2dd2d94922bd44436224fe80b797
pooleside <lpoole26@gmail.com>
2015-10-15 11:49:07 -0700

    "add new component and update function"

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,18 +5,11 @@ export default Ember.Route.extend({
     return this.store.findAll('question');
 
   },
-
-  actions: {
+    actions: {
     save(params) {
       var newQuestion=this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-    },
-    delete(question) {
-      if(confirm('Are you sure you want to delete this question?')){
-      question.destroyRecord();
-      this.transitionTo('index');
-     }
-   }
+    }
   }
 });
